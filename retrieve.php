410e19f3e0ebef4a50e68f3ac65df88e268fd46a
Nick Francis Bundal <nnikeetmg@gmail.com>
2023-12-26 00:40:08 +0800

    add query for employee num

--- retrieve.php
+++ retrieve.php
@@ -20,7 +20,7 @@ if ($resultTotalSalary->num_rows > 0) {
     }
 }
 
-// Query 2: Average salary by department (example)
+// Query 2: Average salary by department
 $sqlAvgSalary = "SELECT ROUND(AVG(service_records.monthly_salary)) AS avg_salary, departments.dept_name 
                  FROM service_records
                  INNER JOIN employees ON employees.idemployees = service_records.employees_idemployees
@@ -38,10 +38,28 @@ if ($resultAvgSalary->num_rows > 0) {
     }
 }
 
+// Query 3: Number of employees per department
+$sqlEmployeeCount = "SELECT COUNT(employees.idemployees) AS employee_count, departments.dept_name
+                    FROM employees
+                    INNER JOIN employees_unitassignments ON employees.idemployees = employees_unitassignments.employees_idemployees
+                    INNER JOIN departments ON departments.iddepartments = employees_unitassignments.departments_iddepartments
+                    GROUP BY departments.dept_name";
+
+$resultEmployeeCount = $conn->query($sqlEmployeeCount);
+
+$dataEmployeeCount = array();
+
+if ($resultEmployeeCount->num_rows > 0) {
+    while ($row = $resultEmployeeCount->fetch_assoc()) {
+        $dataEmployeeCount[] = $row;
+    }
+}
+
 // Combine the results into a single array
 $responseData = array(
     'total_salary' => $dataTotalSalary,
-    'avg_salary' => $dataAvgSalary
+    'avg_salary' => $dataAvgSalary,
+    'employee_count' => $dataEmployeeCount
 );
 
 header('Content-Type: application/json');
@@ -49,4 +67,3 @@ echo json_encode($responseData);
 
 $conn->close();
 ?>
-
